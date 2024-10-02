1d8212dca8c1e3d6df26cf1ad2027510645f1189
Luca Nardelli <lucanardelli91@gmail.com>
2024-10-02 10:22:37 +0200

    bybit: only parse bidsasks if requested by the user

--- ts/src/pro/bybit.ts
+++ ts/src/pro/bybit.ts
@@ -168,6 +168,8 @@ export default class bybit extends bybitRest {
         });
     }
 
+    isWatchingBidsAsks: boolean = false;
+
     requestId () {
         this.lockId ();
         const requestId = this.sum (this.safeInteger (this.options, 'requestId', 0), 1);
@@ -620,6 +622,7 @@ export default class bybit extends bybitRest {
      * @returns {object} a [ticker structure]{@link https://docs.ccxt.com/?id=ticker-structure}
      */
     async watchBidsAsks (symbols: Strings = undefined, params = {}): Promise<Tickers> {
+        this.isWatchingBidsAsks = true;
         await this.loadMarkets ();
         symbols = this.marketSymbols (symbols, undefined, false);
         const messageHashes = [];
@@ -1009,15 +1012,15 @@ export default class bybit extends bybitRest {
         const symbol = market['symbol'];
         const timestamp = this.safeInteger (message, 'ts');
         if (!(symbol in this.orderbooks)) {
-            this.orderbooks[symbol] = this.orderBook({}, this.parseNumber(limit));
+            this.orderbooks[symbol] = this.orderBook ({}, this.parseNumber (limit));
         }
         const orderbook = this.orderbooks[symbol];
         orderbook['symbol'] = symbol;
         if (isSnapshot) {
             // Reset book if depth of snapshot is higher than actual one, so that we can adapt to the new depth
-            const depth = this.parseNumber(limit);
-            if(depth > orderbook.depth) {
-                this.orderbooks[symbol] = this.orderBook({}, depth);
+            const depth = this.parseNumber (limit);
+            if (depth > orderbook.depth) {
+                this.orderbooks[symbol] = this.orderBook ({}, depth);
             }
             const snapshot = this.parseOrderBook (data, symbol, timestamp, 'b', 'a');
             orderbook.reset (snapshot);
@@ -1032,7 +1035,7 @@ export default class bybit extends bybitRest {
         const messageHash = 'orderbook' + ':' + symbol;
         this.orderbooks[symbol] = orderbook;
         client.resolve (orderbook, messageHash);
-        if (limit === '1') {
+        if (limit === '1' && this.isWatchingBidsAsks) {
             const bidask = this.parseWsBidAsk (this.orderbooks[symbol], market);
             const newBidsAsks: Dict = {};
             newBidsAsks[symbol] = bidask;

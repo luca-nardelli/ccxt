a98229e115d7d8a5d9fa69722ac3427d68d5f90d
Luca Nardelli <lucanardelli91@gmail.com>
2025-03-02 23:50:44 +0100

    Remove iso8601 from parseTicker

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -2326,7 +2326,7 @@ export default class bybit extends Exchange {
         return this.safeTicker ({
             'symbol': symbol,
             'timestamp': timestamp,
-            'datetime': this.iso8601 (timestamp),
+            // 'datetime': this.iso8601 (timestamp),
             'high': high,
             'low': low,
             'bid': bid,
